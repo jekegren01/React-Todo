c57ef9f1ef1063ffa73a8a17d3167e6de3aa4316
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-08-05 18:10:36 -0700

    Set up, completed basic form in App.js, parent component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import './components/Todo.css';
+
+const todos = {
+  name: '',
+  id: '',
+  completed: false
+}
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+  }
+
+  //class method to update state
+  toggleTodo = todoId => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todoId === todo.id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    });
+  };
+
+  //class method to add a todo item
+  addTodo = todo => {
+    const newTodo = {
+      name: todo,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm />
+        <TodoList />
       </div>
     );
   }
